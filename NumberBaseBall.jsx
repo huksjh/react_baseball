b3af635fd52bd5a4629943ce0c4209168d5dfcad
Your Name <you@example.com>
2020-04-15 21:28:44 +0900

    111 삭제

--- NumberBaseBall.jsx
+++ NumberBaseBall.jsx
@@ -88,12 +88,12 @@ class NumberBaseBall extends Component {
         return (
             <div>
                 <h1>
-                    111최대 게임회수 {maxCnt} / 입력 회수 {in_Cnt}
+                    최대 게임회수 {maxCnt} / 입력 회수 {in_Cnt}
                 </h1>
                 <form onSubmit={this.onSubmitForm}>
                     <input type="text" value={user_value} onChange={this.onChangeInput} placeholder="값을 입력하세요" maxLength={4} />
                 </form>
-                <h1>결과11111</h1>
+                <h1>결과</h1>
 
                 <ul>
                     {input_log.map((val, index) => {
